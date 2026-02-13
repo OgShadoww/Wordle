3e2883979b5acaa3ca52b3db8a1412b12227fddb
Orest <galenzao@gmail.com>
2026-02-13 19:15:30 +0000

    Clean helper

--- src/tools/helpers.c
+++ src/tools/helpers.c
@@ -19,24 +19,3 @@ uint64_t get_current_ms() {
 
   return (uint64_t)tv.tv_sec * 1000ULL + (uint64_t)tv.tv_usec / 1000ULL;
 }
-
-void print_num(int n) {
-  char buff[32];
-  int i = 0;
-
-  while (n > 0) {
-    buff[i++] = '0' + (n % 10);
-    n /= 10;
-  }
-  buff[i] = '\0';
-
-  for (int j = 0; j < i / 2; j++) {
-    char tmp = buff[j];
-    buff[j] = buff[i - 1 - j];
-    buff[i - 1 - j] = tmp;
-  }
-  
-  printf("%s", buff);
-  fflush(stdout);
-  return;
-}
